11330890dadfe83c08d3afb8148b3e701c02693c
Wael Wahbeh <88880103+peace2074@users.noreply.github.com>
2021-12-27 08:16:50 +0200

    start 3

--- src/.d.ts
+++ src/.d.ts
@@ -1,1 +1,46 @@
+export enum StorePluginTypes {
+  'localStorage' = 'localStorage',
+  'sessionStorage' = 'sessionStorage',
+  'memory' = 'memory',
+  'vuex' = 'vuex',
+  'pinia' = 'pinia',
+  'gun' = 'gun'
+}
+export enum StoreTypes {
+  localStorage = 'localStorage',
+  sessionStorage = 'sessionStorage',
+  memory = 'memoryStorage',
+  vuex = 'vuexStorage',
+  pinia = 'piniaStorage',
+  gun = 'gunStorage'
+}
+export interface StoreOptions {
+  type: string;
+}
+export interface GetItem {
+  (key: string): string | object | string[] | object[] | null | boolean;
+}
+export interface SetItem {
+  (key: string, value: string | object | string[] | object[]): void | boolean;
+}
+export interface RemoveItem {
+  (key: string): void | boolean;
+}
+export interface UniversalStoreClass {
+  type?: string;
+  _storage?: Storage;
+  get: GetItem;
+  set: SetItem;
+  remove?: RemoveItem;
+}
 
+export namespace globalThis {
+  interface Window {
+    localStorage: UniversalStoreClass;
+    sessionStorage: UniversalStoreClass;
+    memoryStorage: UniversalStoreClass;
+    vuexStorage: UniversalStoreClass;
+    piniaStorage: UniversalStoreClass;
+    gunStorage: UniversalStoreClass;
+  }
+}
--- src/index.ts
+++ src/index.ts
@@ -1,57 +1,31 @@
-import { localStorage } from "./vendors/localStorage";
-import { memoryStorage } from "./vendors/memoryStorage";
-import { sessionStorage } from "./vendors/sessionStorage";
+import { localStorage } from './vendors/localStorage';
+import { sessionStorage } from './vendors/sessionStorage';
+import { memoryStorage } from './vendors/memoryStorage';
+import { vuexStorage } from './vendors/vuexStorage';
+import { piniaStorage } from './vendors/piniaStorage';
+import { gunStorage } from './vendors/gunStorage';
+import { StoreTypes, StorePluginTypes, UniversalStoreClass } from './.d';
 
-class IStore implements IStoreClass {
-  public StoreType: string = 'localStorage';
-  constructor(options: StoreOptions) {
-    console.log(this.getAssociatedStoreTypeFromAny(options)); /*?*/
+const initialStore = StoreTypes.localStorage;
+export const UniversalStore = (
+  storePluginType: Partial<StoreTypes> = initialStore
+): UniversalStoreClass => {
+  switch (storePluginType) {
+    case StoreTypes.localStorage:
+      return localStorage;
+    case StoreTypes.sessionStorage:
+      return sessionStorage;
+    case StoreTypes.memory:
+      return memoryStorage;
+    case StoreTypes.vuex:
+      return vuexStorage;
+    case StoreTypes.pinia:
+      return piniaStorage;
+    case StoreTypes.gun:
+      return gunStorage;
+    default:
+      return localStorage;
   }
-
-  getAssociatedStoreTypeFromString(store_type: string): StoreTypes {
-    switch (true) {
-      case store_type === StoreTypes.localStorage:
-        return localStorage
-      case store_type === StoreTypes.sessionStorage:
-        return sessionStorage
-      case store_type === StoreTypes.memory:
-        return memoryStorage
-      case store_type === StoreTypes.vuex:
-        return StoreTypes.vuex;
-      case store_type === StoreTypes.pinia:
-        return StoreTypes.pinia;
-      case store_type === StoreTypes.gun:
-        return StoreTypes.gun;
-      default:
-        return StoreTypes.localStorage;
-    }
-  }
-
-  getAssociatedStoreTypeFromAny(storeType: any): StoreTypes {
-    switch (typeof storeType) {
-      case 'string':
-        return this.getAssociatedStoreTypeFromString(storeType);
-      case 'object':
-        return storeType.type
-          ? this.getAssociatedStoreTypeFromString(storeType.type)
-          : StoreTypes.localStorage;
-      default:
-        if (Array.isArray(storeType)) {
-          const testType: string | null = storeType[0] && storeType[0].type;
-          return testType
-            ? this.getAssociatedStoreTypeFromString(testType)
-            : StoreTypes.localStorage;
-        }
-        return StoreTypes.localStorage;
-    }
-  }
-  get(key: string): string | object | string[] | object[] | null {
-    return 'null';
-  }
-  set(key: string, value: string | object | string[] | object[]): void {
-    return;
-  }
-}
-var test = new IStore({ type: 'localStorage' });
+};
+var test = UniversalStore(StoreTypes.localStorage);
 test; /*?*/
-export class Store extends IStore {}
