4d014cc5f0a014d0de89448a00cbfdcd0979ec70
Wael Wahbeh <88880103+peace2074@users.noreply.github.com>
2022-11-02 15:46:46 +0200

    update: secureStorage

--- __tests__/8.Secure.test.ts
+++ __tests__/8.Secure.test.ts
@@ -1,8 +1,14 @@
-import { uStore } from '../index';
+import { uStore, secureStorage } from '../index';
 
-test('Memory Stotage', () => {
+describe('Memory Stotage', () => {
   const payload = 'Test Payload1';
   const label = 'test';
   uStore.secure.set(label, payload);
-  expect(uStore.secure.getItem(label)).toEqual(payload);
+  test('uStore set & get', () => {
+    expect(uStore.secure.get(label)).toEqual(payload);
+  });
+  secureStorage.set(label, payload);
+  test('secureStorage set & get', () => {
+    expect(secureStorage.get(label)).toEqual(payload);
+  });
 });
--- src/stores/secureStorage.ts
+++ src/stores/secureStorage.ts
@@ -1,5 +1,5 @@
-import { _encrypt, _decrypt } from "waelio-utils";
-import { uStore } from "../.d";
+import { _encrypt, _decrypt } from 'waelio-utils';
+import { uStore } from '../.d';
 const memoryStore = {};
 
 const secureStorage: uStore = {
@@ -7,11 +7,11 @@ const secureStorage: uStore = {
     return memoryStore;
   },
   getItem: function (key: string) {
-    const test = memoryStore[key];
-    return test ? _decrypt(test) : key;
+    return _decrypt(memoryStore[key])
   },
   set: function (key, value) {
     memoryStore[key] = _encrypt(value);
+    return { [key]: memoryStore[key] };
   },
   has(key: string): string | boolean {
     return memoryStore[key] ?? false;
@@ -19,18 +19,18 @@ const secureStorage: uStore = {
   remove(key: string) {
     delete memoryStore[key];
     return this.has(key);
-  },
+  }
 };
 
 const descriptor = Object.create({});
-descriptor.value = "readonly";
+descriptor.value = 'readonly';
 
-Object.defineProperty(secureStorage, "myStore", descriptor);
+Object.defineProperty(secureStorage, 'myStore', descriptor);
 
-Object.defineProperty(secureStorage, "_data", {
+Object.defineProperty(secureStorage, '_data', {
   enumerable: false,
   configurable: false,
-  writable: false,
+  writable: false
 });
 export { secureStorage };
 export default secureStorage;
