55059f27832716d6abf17fe9a45268f77c9787f4
Wael Wahbeh <88880103+peace2074@users.noreply.github.com>
2022-11-08 19:59:35 +0200

    feature: reinstate config storage

--- __tests__/9.Config.test.ts
+++ __tests__/9.Config.test.ts
@@ -1,10 +1,11 @@
+import { describe, expect, test } from '@jest/globals';
 import { uStore, configStorage } from '../index';
 
 const payload = 'Test Payload1';
 const label = 'test';
 
-describe('Config Stotage', () => {
-  uStore.secure.set(label, payload);
+describe('uStore Storage', () => {
+  uStore.config.set(label, payload);
   test('uStore set & get', () => {
     expect(uStore.config.getItem(label)).toEqual(payload);
   });
