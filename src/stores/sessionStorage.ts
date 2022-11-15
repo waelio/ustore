1ad8ae7b166bf51b4f971c890f40af3d30afc7db
Wael Wahbeh <88880103+peace2074@users.noreply.github.com>
2022-11-15 11:19:00 +0200

    update: update store name

--- src/stores/sessionStorage.ts
+++ src/stores/sessionStorage.ts
@@ -1,7 +1,7 @@
 import store from "store2";
 import { UStoreClass } from "../.d";
 
-const session = store.session.namespace("uStore");
+const session = store.session.namespace("uStore:sessionStorage");
 export const sessionStorage: UStoreClass = {
   get: (key: string) => session.get(key),
   set: (key: string, value: any) => session.set(key, value),
