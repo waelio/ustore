61db137f5a0c726e34c09cb3c7fc897509355410
Wael Wahbeh <88880103+peace2074@users.noreply.github.com>
2022-11-15 11:19:32 +0200

    feature: idbStorage 1

--- src/stores/idbStorage.ts
+++ src/stores/idbStorage.ts
@@ -1,51 +1,86 @@
-import { UStoreClass } from '../types';
-// @ts-nocheck
+import { UStoreClass } from "../types";
+import LocalForage from "localforage";
+const NAME = 'idbStorage';
+LocalForage.config({
+  driver: LocalForage.INDEXEDDB,
+  name: NAME,
+  version: 1.0,
+  size: 4980736,
+  storeName: NAME,
+  description: 'uStore.idbStorage'
+});
+let store: LocalForage
 
+try {
+  store = LocalForage.createInstance({
+    name: NAME
+  });
+} catch (error) {
+  console.log(error);
+}
+import { _to } from "waelio-utils";
 
-// import { _encrypt, _decrypt } from "waelio-utils";
-// import { NAME }               from '../../loadLocalbase.mjs'
-import { STORENAME } from '../../loadLocalbase.mjs'
-import { db } from '../../loadLocalbase.mjs'
-const store = db.collection(STORENAME)
-
-export const idbStorage: Partial<UStoreClass> = ({
-  get: (key: string) => {
+export const idbStorage: UStoreClass = {
+  get: async (key: string) => {
     try {
-      return store.get(key);
+      //@ts-ignore
+      const [error, success] = await _to(store.getItem(key));
+      if (error) {
+        throw "Error getting item"
+      }
+      return success
     } catch (error: unknown) {
-      return error || null;
+      alert(error)
+      return error;
     }
   },
-  getItem: (key: string) => {
+  getItem: async (key: string) => {
     try {
-      return store.get(key);
+      //@ts-ignore
+      const [error, success] = await _to(store.getItem(key));
+      if (error) {
+        return "Error getting item"
+      }
+      return success
     } catch (error: unknown) {
-      return error || null;
-    };
+      alert(error)
+      return error;
+    }
   },
-  set: (key: any, value: any) => {
+  set: async (key: any, value: any) => {
     try {
-      return store(key).add(value) as void
+      const [error, success] = await _to(store.setItem(key, value));
+      if (error) {
+        return "Error getting item"
+      }
+      return success
     } catch (error: unknown) {
-      return error
+      alert(error)
+      return error;
     }
   },
   //@ts-ignore
   has: function (key: string): boolean {
     try {
-      return Boolean(store.get(key));
+      return Boolean(store.getItem(key));
     } catch (_) {
+      alert(_)
       return false;
     }
   },
-  remove: (key: string) => {
+  remove: async (key: string) => {
     try {
-      db.collection(key).remove();
-      return store.get(key) as string | boolean
+      //@ts-ignore
+      const [error, _] = await _to(store.removeItem(key))
+      if (error) {
+        return "Error getting item"
+      }
+      return await store.getItem(key) as string | boolean;
     } catch (error) {
+      alert(error)
       return error;
     }
-  }
-});
+  },
+};
 
 export default idbStorage;
