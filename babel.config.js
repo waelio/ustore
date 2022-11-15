a789c850230b3811082bb0d15ef538b076da3023
Wael Wahbeh <88880103+peace2074@users.noreply.github.com>
2022-11-15 11:21:11 +0200

    update: update packages

--- babel.config.js
+++ babel.config.js
@@ -2,4 +2,26 @@ module.exports = {
   presets: [
     ['@babel/preset-env', { targets: { node: 'current' } }],
   ],
+  transform: {
+    "\\.[tj]sx?$": ["babel-jest", {
+      presets: [
+        ["@babel/preset-env",
+          {
+            targets: {
+              "node": 10
+            }
+          }
+        ]
+      ],
+      plugins: [
+        "@babel/plugin-proposal-class-properties",
+        "@babel/plugin-transform-typescript",
+        "add-module-exports"
+      ]
+    }]
+  },
+  transformFile:{
+    filename: "./idbStorage.js",
+    sourceRoot: "./Local.ts"
+  }
 };
