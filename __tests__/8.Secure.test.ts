64f254301f1bf2e861464f0c253bf5bc713768e0
Wael Wahbeh <88880103+peace2074@users.noreply.github.com>
2023-02-24 21:21:12 +0200

    update secure storage typing

--- __tests__/8.Secure.test.ts
+++ __tests__/8.Secure.test.ts
@@ -4,14 +4,23 @@ jest.mock('localforage')
 
 const payload = 'Test Payload1';
 const label = 'test';
+const options = { salt: 'ustore-salt'}
 
 describe('Secure Storage', () => {
   uStore.secure.set(label, payload);
   test('uStore set & get', () => {
     expect(uStore.secure.getItem(label)).toEqual(payload);
   });
-  secureStorage.set(label, payload);
+  test('uStore set & get', () => {
+    uStore.secure.set(label, payload, options);
+    expect(uStore.secure.getItem(label, options)).toEqual(payload);
+  });
   test('secureStorage set & get', () => {
+    secureStorage.set(label, payload);
     expect(secureStorage.getItem(label)).toEqual(payload);
+  });
+  test('secureStorage set & get', () => {
+    secureStorage.set(label, payload, options);
+    expect(secureStorage.getItem(label, options)).toEqual(payload);
   });
 });
