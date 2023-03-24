19ecc738cee687e65ee2d6838aaa2972a59e00f6
Wael Wahbeh <88880103+peace2074@users.noreply.github.com>
2023-03-25 00:06:08 +0300

    fix localforage again2

--- src/_stores/idbStorage.ts
+++ src/_stores/idbStorage.ts
@@ -1,11 +1,14 @@
-import * as memoryDriver from "localforage-driver-memory";
 import * as localforage from "localforage";
+import memoryStorage from "./memoryStorage";
 const NAME = "idbStorage";
-let store: LocalForage;
-
-try {
+let store: any;
+let idbStorage: any;
+if (localforage.supports(localforage.INDEXEDDB)) {
   localforage.config({
-    driver: localforage.INDEXEDDB,
+    driver: [
+      localforage.INDEXEDDB,
+      localforage.LOCALSTORAGE
+    ],
     name: NAME,
     version: 1.0,
     size: 4980736,
@@ -15,121 +18,123 @@ try {
   store = localforage.createInstance({
     name: NAME,
   });
-  localforage.defineDriver(memoryDriver);
-  localforage.setDriver(memoryDriver._driver);
-} catch (error) {}
 
-export { store };
+  store = localforage
 
-export const idbStorage = {
-  get: async (key: string) => {
-    try {
-      return store.ready().then(() => {
-        store.getItem(key, (err: any, value: any) => {
-          if (!!err) {
-            return "Error getting item";
-          }
-          return value;
-        });
-      });
-    } catch (error: unknown) {
-      return error;
-    }
-  },
-  getItem: async (key: string) => {
-    try {
-      return store.ready().then(() => {
-        return store.getItem(key, (err: any, value: unknown) => {
-          if (!!err) {
-            return "Error getting item";
-          }
-          return value;
-        });
-      });
-    } catch (error: unknown) {
-      return error;
-    }
-  },
-  set: async (key: any, value: any): Promise<any> => {
-    try {
-      store.ready().then(() => {
-        return store.setItem(key, value, function (err: unknown) {
-          if (err) {
-            return "Error getting item";
-          }
+  idbStorage = {
+    get: async (key: string) => {
+      try {
+        return store.ready().then(() => {
           store.getItem(key, (err: any, value: any) => {
-            if (err) {
+            if (!!err) {
               return "Error getting item";
             }
             return value;
           });
         });
-      });
-    } catch (error: unknown) {
-      return error;
-    }
-  },
-  setItem: async (key: any, value: any): Promise<any> => {
-    try {
-      store.ready().then(() => {
-        return store.setItem(key, value, function (err: unknown) {
-          if (err) {
-            return "Error getting item";
-          }
-          store.getItem(key, (err: any, value: any) => {
-            if (err) {
+      } catch (error: unknown) {
+        return error;
+      }
+    },
+    getItem: async (key: string) => {
+      try {
+        return store.ready().then(() => {
+          return store.getItem(key, (err: any, value: unknown) => {
+            if (!!err) {
               return "Error getting item";
             }
             return value;
           });
         });
-      });
-    } catch (error: unknown) {
-      return error;
-    }
-  },
-  has: async (key: string): Promise<boolean> => {
-    try {
-      await store.ready();
-      return await (store.getItem(key) as Promise<boolean>);
-    } catch (_err) {
-      return false;
-    }
-  },
-  hasItem: async (key: string): Promise<boolean> => {
-    try {
-      await store.ready();
-      return await (store.getItem(key) as Promise<boolean>);
-    } catch (_err) {
-      return false;
-    }
-  },
-  remove: async (key: string) => {
-    return store
-      .ready()
-      .then(async () => {
-        store.removeItem(key);
-        console.log("Key is cleared!");
-        const value = await store.getItem(key);
-        return value;
-      })
-      .catch((error: any) => {
+      } catch (error: unknown) {
         return error;
-      });
-  },
-  removeItem: async (key: string) => {
-    return store
-      .ready()
-      .then(async () => {
-        store.removeItem(key);
-        console.log("Key is cleared!");
-        const value = await store.getItem(key);
-        return value;
-      })
-      .catch((error: any) => {
+      }
+    },
+    set: async (key: any, value: any): Promise<any> => {
+      try {
+        store.ready().then(() => {
+          return store.setItem(key, value, function (err: unknown) {
+            if (err) {
+              return "Error getting item";
+            }
+            store.getItem(key, (err: any, value: any) => {
+              if (err) {
+                return "Error getting item";
+              }
+              return value;
+            });
+          });
+        });
+      } catch (error: unknown) {
         return error;
-      });
-  },
-};
+      }
+    },
+    setItem: async (key: any, value: any): Promise<any> => {
+      try {
+        store.ready().then(() => {
+          return store.setItem(key, value, function (err: unknown) {
+            if (err) {
+              return "Error getting item";
+            }
+            store.getItem(key, (err: any, value: any) => {
+              if (err) {
+                return "Error getting item";
+              }
+              return value;
+            });
+          });
+        });
+      } catch (error: unknown) {
+        return error;
+      }
+    },
+    has: async (key: string): Promise<boolean> => {
+      try {
+        await store.ready();
+        return await (store.getItem(key) as Promise<boolean>);
+      } catch (_err) {
+        return false;
+      }
+    },
+    hasItem: async (key: string): Promise<boolean> => {
+      try {
+        await store.ready();
+        return await (store.getItem(key) as Promise<boolean>);
+      } catch (_err) {
+        return false;
+      }
+    },
+    remove: async (key: string) => {
+      return store
+        .ready()
+        .then(async () => {
+          store.removeItem(key);
+          console.log("Key is cleared!");
+          const value = await store.getItem(key);
+          return value;
+        })
+        .catch((error: any) => {
+          return error;
+        });
+    },
+    removeItem: async (key: string) => {
+      return store
+        .ready()
+        .then(async () => {
+          store.removeItem(key);
+          console.log("Key is cleared!");
+          const value = await store.getItem(key);
+          return value;
+        })
+        .catch((error: any) => {
+          return error;
+        });
+    },
+  };
 
-export default idbStorage;
+
+} else {
+  store = memoryStorage
+}
+export { store as idbStorage };
+export default idbStorage = store;
--- src/_stores/webqlStorage.ts
+++ src/_stores/webqlStorage.ts
@@ -1,12 +1,15 @@
 import * as localforage from "localforage";
-import * as memoryDriver from "localforage-driver-memory";
+import memoryStorage from "./memoryStorage";
 
-import { UStoreClass } from "../.d";
 const NAME = "webqlStorage";
-let store;
-try {
+let store: any;
+let webqlStorage: any;
+if (localforage.supports(localforage.INDEXEDDB)) {
   localforage.config({
-    driver: localforage.WEBSQL,
+    driver: [
+      localforage.WEBSQL,
+      localforage.LOCALSTORAGE
+    ],
     name: NAME,
     version: 1.0,
     size: 4980736,
@@ -16,130 +19,130 @@ try {
   store = localforage.createInstance({
     name: NAME,
   });
-  localforage.defineDriver(memoryDriver);
-  localforage.setDriver(memoryDriver._driver);
   store = localforage;
-} catch (error) {}
-
-export const webqlStorage: UStoreClass = {
-  get: async (key: string) => {
-    try {
-      return store
-        .ready()
-        .then(() => {
-          store.getItem(key, (err: any, value: unknown) => {
-            if (!!err) {
-              return "Error getting item";
-            }
-            return value;
+  webqlStorage = {
+    get: async (key: string) => {
+      try {
+        return store
+          .ready()
+          .then(() => {
+            store.getItem(key, (err: any, value: unknown) => {
+              if (!!err) {
+                return "Error getting item";
+              }
+              return value;
+            });
+          })
+          .catch((err: any) => {
+            return err;
           });
-        })
-        .catch((err: any) => {
-          return err;
-        });
-    } catch (error: unknown) {
-      return error;
-    }
-  },
-  getItem: async (key: string) => {
-    try {
-      return store
-        .ready()
-        .then(() => {
-          store.getItem(key, (err: any, value: unknown) => {
-            if (!!err) {
-              return "Error getting item";
-            }
-            return value;
+      } catch (error: unknown) {
+        return error;
+      }
+    },
+    getItem: async (key: string) => {
+      try {
+        return store
+          .ready()
+          .then(() => {
+            store.getItem(key, (err: any, value: unknown) => {
+              if (!!err) {
+                return "Error getting item";
+              }
+              return value;
+            });
+          })
+          .catch((err: any) => {
+            return err;
           });
-        })
-        .catch((err: any) => {
-          return err;
-        });
-    } catch (error: unknown) {
-      return error;
-    }
-  },
-  set: async (key: any, value: any): Promise<any> => {
-    try {
-      return store.ready().then(() => {
-        store.setItem(key, value, function (err: unknown) {
-          if (err) {
-            return "Error getting item";
-          }
-          store.getItem(key, (err: any, value: any) => {
+      } catch (error: unknown) {
+        return error;
+      }
+    },
+    set: async (key: any, value: any): Promise<any> => {
+      try {
+        return store.ready().then(() => {
+          store.setItem(key, value, function (err: unknown) {
             if (err) {
               return "Error getting item";
             }
-            return value;
+            store.getItem(key, (err: any, value: any) => {
+              if (err) {
+                return "Error getting item";
+              }
+              return value;
+            });
           });
         });
-      });
-    } catch (error: unknown) {
-      return error;
-    }
-  },
-  setItem: async (key: any, value: any): Promise<any> => {
-    try {
-      return store.ready().then(() => {
-        return store.setItem(key, value, function (err: unknown) {
-          if (err) {
-            return "Error getting item";
-          }
-          store.getItem(key, (err: any, value: any) => {
+      } catch (error: unknown) {
+        return error;
+      }
+    },
+    setItem: async (key: any, value: any): Promise<any> => {
+      try {
+        return store.ready().then(() => {
+          return store.setItem(key, value, function (err: unknown) {
             if (err) {
               return "Error getting item";
             }
+            store.getItem(key, (err: any, value: any) => {
+              if (err) {
+                return "Error getting item";
+              }
+              return value;
+            });
+          });
+        });
+      } catch (error: unknown) {
+        return error;
+      }
+    },
+    has: (key: string): boolean => {
+      try {
+        return Boolean(store.ready().then(() => store.getItem(key)));
+      } catch (_) {
+        alert(_);
+        return false;
+      }
+    },
+    hasItem: (key: string): boolean => {
+      try {
+        return Boolean(store.ready().then(() => store.getItem(key)));
+      } catch (_) {
+        alert(_);
+        return false;
+      }
+    },
+    remove: async (key: string) => {
+      try {
+        return store.ready().then(async () => {
+          await store.removeItem(key);
+          console.log("Key is cleared!");
+          return store.getItem(key).then((value: any) => {
             return value;
           });
         });
-      });
-    } catch (error: unknown) {
-      return error;
-    }
-  },
-  has: (key: string): boolean => {
-    try {
-      return Boolean(store.ready().then(() => store.getItem(key)));
-    } catch (_) {
-      alert(_);
-      return false;
-    }
-  },
-  hasItem: (key: string): boolean => {
-    try {
-      return Boolean(store.ready().then(() => store.getItem(key)));
-    } catch (_) {
-      alert(_);
-      return false;
-    }
-  },
-  remove: async (key: string) => {
-    try {
-      return store.ready().then(async () => {
-        await store.removeItem(key);
-        console.log("Key is cleared!");
-        return store.getItem(key).then((value: any) => {
-          return value;
+      } catch (error) {
+        return error;
+      }
+    },
+    removeItem: async (key: string) => {
+      try {
+        return store.ready().then(async () => {
+          await store.removeItem(key);
+          console.log("Key is cleared!");
+          return store.getItem(key).then((value: any) => {
+            return value;
+          });
         });
-      });
-    } catch (error) {
-      return error;
-    }
-  },
-  removeItem: async (key: string) => {
-    try {
-      return store.ready().then(async () => {
-        await store.removeItem(key);
-        console.log("Key is cleared!");
-        return store.getItem(key).then((value: any) => {
-          return value;
-        });
-      });
-    } catch (error) {
-      return error;
-    }
-  },
-};
+      } catch (error) {
+        return error;
+      }
+    },
+  };
+} else {
+  store = memoryStorage
+}
 
-export default webqlStorage;
+export { store as webqlStorage};
+export default webqlStorage = store;;
