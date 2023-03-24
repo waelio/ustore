913e0aacc0b52136ee22bd0e50c7ce09b5f44eb1
Wael Wahbeh <88880103+peace2074@users.noreply.github.com>
2023-03-25 00:08:41 +0300

    fix localforage again2

--- lib/src/config/index.ts
+++
@@ -1,8 +0,0 @@
-import client from "./client";
-import dev from "./dev";
-import prod from "./prod";
-import server from "./server";
-const CONFIG = () => ({ server, client, dev, prod });
-
-export { server, client, dev, prod, CONFIG };
-export default CONFIG;
